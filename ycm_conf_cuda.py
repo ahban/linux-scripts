150d400ce42802688f2a8c1d25e72b6c2b5cdaa1
Ban Zhihua <sawpara@126.com>
2019-01-06 15:57:17 +0800

    update cuda

--- ycm_conf_cuda.py
+++ ycm_conf_cuda.py
@@ -63,6 +63,8 @@ flags = [
 '-I', '/usr/include/c++/4.8.5',
 '-I', '/usr/include',
 '-I', os.popen('which nvcc').read().replace('bin/nvcc\n', 'include'),
+'--cuda-path=' + os.popen('which nvcc').read().replace('bin/nvcc\n', ''),
+'-nocudalib',
 ]
 
 # Clang automatically sets the '-std=' flag to 'c++14' for MSVC 2015 or later,
